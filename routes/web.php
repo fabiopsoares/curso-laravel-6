a493fec1920affaeaba60a81ee1d28091ff5a727
Fabio Pereira Soares <fabiotecninf@gmail.com>
2020-11-14 17:43:00 -0300

    Aula 11

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,45 @@
 <?php
 
+Route::view('/view','welcome');
+
+Route::redirect('redirect1','redirect2');
+
+/*
+Route::get('/redirect1', function(){
+    return redirect('/redirect2');
+});*/
+
+Route::get('redirect2', function(){
+    return 'redirect2';
+});
+
+Route::get('/produtos/{idProduct?}', function($idProduct=""){
+    return "Produtos: $idProduct";
+});
+
+Route::get('/categoria/{flag}/post',function($flag){
+
+    return "Produtos da categoria: $flag";
+});
+
+Route::get('/categorias/{flag}',function($flag){
+
+    return "Produtos da categoria: $flag";
+});
+
+Route::match(['get','post'], '/match', function(){
+    return 'match';
+});
+
+//aceita todo tipo de requisição http exemplo get, post, put tomar cuidado ao usar
+Route::any('any',function(){
+    return 'any';
+});
+
+Route::get('/contato', function(){
+    return view('site.contact');
+});
+
 Route::get('/', function () {
     return view('welcome');
 });
