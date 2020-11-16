74d9e2afbaa58e09114095ccdd47ab49fe1195e4
Fabio Pereira Soares <fabiotecninf@gmail.com>
2020-11-16 11:11:50 -0300

    Aula 14

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,51 @@
 <?php
 
+
+Route::get('/login', function(){
+    return 'Login';
+})->name('login');
+
+Route::group([
+    'middleware' => [],
+    'prefix' => 'admin',
+    'namespace' => 'Admin'
+], function(){
+    Route::name('admin.')->group(function(){
+        Route::get('/dashboard', 'TesteController@teste')->name('dashbord');
+
+        Route::get('/financeiro', 'TesteController@teste')->name('financeiro');
+
+        Route::get('/produtos','TesteController@teste')->name('produtos');
+
+        Route::get('/', function(){
+            return redirect()->route('admin.dashbord');
+        })->name('home');
+    });
+});
+
+/*
+Route::middleware([])->group(function(){
+
+    Route::prefix('admin')->group(function(){
+
+        Route::namespace('Admin')->group(function(){
+
+            Route::name('admin.')->group(function(){
+                Route::get('/dashboard', 'TesteController@teste')->name('dashbord');
+
+                Route::get('/financeiro', 'TesteController@teste')->name('financeiro');
+
+                Route::get('/produtos','TesteController@teste')->name('produtos');
+
+                Route::get('/', function(){
+                    return redirect()->route('admin.dashbord');
+                })->name('home');
+            });
+        });
+    });
+});*/
+
+
 Route::get('redirect3', function(){
     return redirect()->route('url.name');
 });
