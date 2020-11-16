b29e8d3ed77419e7f9a96a71894545abc0b736c2
Fabio Pereira Soares <fabiotecninf@gmail.com>
2020-11-16 15:37:10 -0300

    Aula 19

---
+++ app/Http/Controllers/ProductControllerOld.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    public function index()
+    {
+
+        $products = ['Product 01','Product 02','Product 03'];
+
+        return $products;
+    }
+
+    public function show ($id)
+    {
+        return "Exibindo o produto de id: {$id}";
+    }
+
+    public function create()
+    {
+        return 'Exibindo o form de cadastro de um novo produto';
+    }
+
+    public function edit($id)
+    {
+        return "Form para editar o produto {$id}";
+    }
+
+    public function estor()
+    {
+        return "Cadastrando o novo produto";
+    }
+
+    public function update($id)
+    {
+        return "Editando o produto {$id}";
+    }
+
+    public function destroy($id)
+    {
+        return "Deletando o produto {$id}";
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,20 @@
 <?php
 
+Route::get('/login', function(){
+    return 'Login';
+})->name('login');
+
+Route::resource('products','ProductController');
+
+/*
 Route::delete('products/{id}','ProductController@destroy')->name('products.destroy');
 Route::put('products/{id}','ProductController@update')->name('products.update');
 Route::get('products/{id}/edit','ProductController@edit')->name('products.edit');
 Route::get('products/create','ProductController@create')->name('products.create');
 Route::get('products/{id}', 'ProductController@show')->name('products.show');
 Route::get('products', 'ProductController@index')->name('products.index');
-Route::post('products', 'ProductController@store')->name('products.store');
+Route::post('products', 'ProductController@store')->name('products.store');*/
 
-
-Route::get('/login', function(){
-    return 'Login';
-})->name('login');
 /*
 Route::group([
     'middleware' => [],
