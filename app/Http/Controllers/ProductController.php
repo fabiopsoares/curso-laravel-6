06cabf982484495c24cbbbc59578bd76cd524971
Fabio Pereira Soares <fabiotecninf@gmail.com>
2020-11-17 09:26:01 -0300

    Aula 24

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -26,7 +26,7 @@ class ProductController extends Controller
     public function index()
     {
         $teste = '<H1>Olá</h1>';
-        return view('teste',compact('teste'));
+        return view('admin.pages.products.index',compact('teste'));
     }
 
     /**
