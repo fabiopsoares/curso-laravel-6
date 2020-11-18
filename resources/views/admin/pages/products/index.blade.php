44b2ce36d0571cf75a7a5065c614ce7b17e5ac6d
Fabio Pereira Soares <fabiotecninf@gmail.com>
2020-11-18 11:49:00 -0300

    44 - Incluindo o Bootstrap no Laravel 6.x (via CDN)

--- resources/views/admin/pages/products/index.blade.php
+++ resources/views/admin/pages/products/index.blade.php
@@ -4,8 +4,8 @@
 @section('content')
 
 <h1>Exibindo os produtos</h1>
-<a href="{{ route('products.create') }}"> Cadastrar Produto</a>
-
+<a href="{{ route('products.create') }}" class="btn btn-primary"> Cadastrar Produto</a>
+{{--
 @include('admin.includes.alerts', ['content' => 'Alerta de preços de produtos'])
 
 <hr>
@@ -15,16 +15,14 @@
         <h1>Titulo card</h1>
     @endslot
     um card de exemplo
-@endcomponent
+@endcomponent --}}
 
-<hr>
-
-<table border="1">
+<table class="table .table-striped ">
     <thead>
         <tr>
             <th>Nome</th>
             <th>Preço</th>
-            <th>Ações</th>
+            <th width="100">Ações</th>
         </tr>
     </thead>
     <tbody>
