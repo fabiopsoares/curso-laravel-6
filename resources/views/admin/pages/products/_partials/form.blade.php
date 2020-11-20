50878868ed83a0eb7d36cdc56486587140bb473a
Fabio Pereira Soares <fabiotecninf@gmail.com>
2020-11-20 09:55:32 -0300

    Ajuste atributo Class bootstrop view form

--- resources/views/admin/pages/products/_partials/form.blade.php
+++ resources/views/admin/pages/products/_partials/form.blade.php
@@ -2,16 +2,16 @@
 
 @csrf
 <div class="form-group">
-    <input type="text" class="form-group" name="name" id="name" placeholder="Nome:" value="{{ $product->name ?? old('name') }}">
+    <input type="text" class="form-control" name="name" id="name" placeholder="Nome:" value="{{ $product->name ?? old('name') }}">
 </div>
 <div class="form-group">
-    <input type="text" class="form-group" name="price" id="price" placeholder="Preço:" value="{{ $product->price ?? old('price') }}">
+    <input type="text" class="form-control" name="price" id="price" placeholder="Preço:" value="{{ $product->price ?? old('price') }}">
 </div>
 <div class="form-group">
-    <input type="text" class="form-group" name="description" id="description" placeholder="Descrição" value="{{ $product->description ?? old('description') }}">
+    <input type="text" class="form-control" name="description" id="description" placeholder="Descrição" value="{{ $product->description ?? old('description') }}">
 </div>
 <div class="form-group">
-    <input type="file" class="form-group" name="image" id="image">
+    <input type="file" class="form-control" name="image" id="image">
 </div>
 <div class="form-group">
     <button type="submit" class="btn btn-success">Enviar</button>
--- resources/views/admin/pages/products/create.blade.php
+++ resources/views/admin/pages/products/create.blade.php
@@ -3,7 +3,7 @@
 @section('title', 'Cadastrar Novo Produto')
 
 @section('content')
-<h1>Cadastrar Movo Produto <a href="{{ route('products.index') }}"><<</a></h1>
+<h1>Cadastrar Novo Produto <a href="{{ route('products.index') }}"><<</a></h1>
 
     <form action="{{route('products.store')}}" method="post" enctype="multipart/form-data" class="form-group">
         @include('admin.pages.products._partials.form')
