5224bc43ba0ee20ba43c72293880b81c5c2a7bf1
Fabio Pereira Soares <fabiotecninf@gmail.com>
2020-11-20 09:13:39 -0300

    54 - Finalizando a Autenticação no Laravel

--- routes/web.php
+++ routes/web.php
@@ -121,4 +121,4 @@ Route::get('/', function () {
 
 /*Auth::routes(['register' => false]);*/
 Auth::routes();
-Route::get('/home', 'HomeController@index')->name('home');
+//Route::get('/home', 'HomeController@index')->name('home');
